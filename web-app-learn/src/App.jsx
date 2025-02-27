c53c72c2f0f94a5db298e74401ef294063976222
Priyaraj05 <riya@DESKTOP-DFEC47U.>
2025-02-26 20:14:54 -0800

    API Setup

--- web-app-learn/src/App.jsx
+++ web-app-learn/src/App.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import Tasks from "./Tasks";
+import Pizza from "./Pizza";
 
 /*const App = () => {
   return React.createElement("div", {}, [
@@ -24,19 +24,29 @@ const root = createRoot(container);
 root.render(React.createElement(App));
 */
 
-
-const App = () =>{
+const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Tasks name = "Task 1" description="This is task 1"/>
-      <Tasks name = "Task 2" description="This is task 2"/>
-      <Tasks name = "Task 3" description="This is task 3"/>
+      <h1>Pizza Corner</h1>
+      <Pizza
+        name="Pizza 1"
+        description="This is italian veggie pizza"
+        image={"public/pizzas/ital_veggie.webp"}
+      />
+      <Pizza
+        name="Pizza 2"
+        description="This is Pizza 2"
+        image={"public/pizzas/veggie_veg.webp"}
+      />
+      <Pizza
+        name="Pizza 3"
+        description="This is Pizza 3"
+        image={"public/pizzas/ckn_pesto.webp"}
+      />
     </div>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App/>);
-
+root.render(<App />);
--- web-app-learn/src/Pizza.jsx
+++ web-app-learn/src/Pizza.jsx
@@ -1,17 +1,18 @@
-// const Tasks = (props) => {
+// const Pizza = (props) => {
 //   return React.createElement("div", {}, [
 //     React.createElement("h2", {}, props.name),
 //     React.createElement("p", {}, props.description),
 //   ]);
 // };
 
-const Tasks = (props) => {
+const Pizza = (props) => {
     return (
-        <div className="task">
+        <div className="pizza">
             <h1>{props.name}</h1>
             <p>{props.description}</p>
+            <img src={props.image} alt={props.name} />
         </div>
     );
 };
 
-export default Tasks;
+export default Pizza;
